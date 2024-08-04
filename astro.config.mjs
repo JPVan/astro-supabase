5f04c96ace8ebc2fe4732b9233ca0dc35a00043b
Jonathan Van Antwerp <11825317+JPVan@users.noreply.github.com>
2024-08-03 20:51:48 -0500

    Update astro config to Cloudflare server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: cloudflare({
+    imageService: "compile",
+  }),
   integrations: [tailwind(), solidJs()],
 });
