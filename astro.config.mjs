1c7fe217f5550265fbd8051c5b6bfadd7404e851
Jonathan Van Antwerp <11825317+JPVan@users.noreply.github.com>
2024-08-06 01:02:02 -0500

    Get building error-free with cloudflare adapter via polyfills

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,37 @@ import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-supabase-auth.vercel.app",
-  output: "server",
+  site: "https://idle.parfait.cloud",
+  output: "hybrid",
   adapter: cloudflare(),
   integrations: [tailwind(), solidJs()],
+
+  vite: {
+    optimizeDeps: {
+      esbuildOptions: {
+        plugins: [
+          NodeGlobalsPolyfillPlugin({
+            buffer: true,
+            process: true
+          }),
+          NodeModulesPolyfillPlugin()
+        ]
+      }
+    },
+    resolve: {
+      alias: {
+        util: 'rollup-plugin-node-polyfills/polyfills/util',
+        path: 'rollup-plugin-node-polyfills/polyfills/path',
+        process: 'process/browser'
+      }
+    },
+    ssr: {
+      noExternal: ['picomatch', 'micromatch']
+    }
+  },
 });
