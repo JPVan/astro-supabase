1f419bd07bc69bd00d82c18c0bd093514ea09766
Jonathan Van Antwerp <11825317+JPVan@users.noreply.github.com>
2024-08-07 12:35:06 -0500

    Fix Supabase env variables, Astro PUBLIC_ prefix are available at build time

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
+// PUBLIC_ prefix makes available at build time in Astro 
 export const supabase = createClient(
-  import.meta.env.SUPABASE_URL,
-  import.meta.env.SUPABASE_ANON_KEY,
+  import.meta.env.PUBLIC_SUPABASE_URL,
+  import.meta.env.PUBLIC_SUPABASE_ANON_KEY,
   {
     auth: {
       flowType: "pkce",
